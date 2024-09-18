79ddeb71eefe90df9ea591fc16289f4aba3a3319
Emma Beldick <emma.beldick@mail.concordia.ca>
2024-09-18 14:17:50 -0400

    Update script.js

--- topics/instructions/pyramids/js/script.js
+++ topics/instructions/pyramids/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(400,400);
 }
 
 
@@ -20,5 +20,39 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+background("#0000ff");
 
+  sand()
+pyramid()
+pyramid2()
+}
+
+function sand(){
+    push();
+    noStroke();
+    fill("yellow");
+    ellipse(100,300,300);
+    pop();
+  
+  //second one
+   push();
+    noStroke();
+    fill("yellow");
+    ellipse(300,300,300);
+    pop();
+}
+
+function pyramid(){
+    push();
+  noStroke();
+  fill("#fee12b");
+  triangle(175,120,108,230,236,225);
+  pop();
+}
+function pyramid2(){
+    push();
+    noStroke();
+    fill("#ffffc5")
+    triangle(175,120,108,230,145,235);
+    pop();
 }
