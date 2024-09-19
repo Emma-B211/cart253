3df46e7552ac8c866d7a6b530029439e7171665e
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-18 21:03:30 -0400

    draw a hole in cheese

--- topics/variables/creating variables/template-p5-project/js/script.js
+++ topics/variables/creating variables/template-p5-project/js/script.js
@@ -12,13 +12,21 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+createCanvas(480,480);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * draws a hole in a piece of cheese
 */
 function draw() {
+//the cheese
+background(255,255,0);
 
+// the hole
+push();
+noStroke();
+fill(0);
+ellipse(140,175,180);
+pop();
 }
