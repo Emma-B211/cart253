ca48983aa773add78b401bbccb7f106de4ddaa75
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-18 20:55:06 -0400

    started flag

--- topics/instructions/flag/flag/js/script.js
+++ topics/instructions/flag/flag/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * Flag
+ * Emma Beldick
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * 
  */
 
 "use strict";
@@ -12,13 +11,42 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
-}
-
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
-
-}
+    createCanvas(600,600);
+    
+    }
+    
+    
+    /**
+     * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+    */
+    function draw() {
+        background("#0000ff");
+         
+      base();
+       greenflag();
+        redflag();
+    }
+    
+    function base(){
+        push();
+        noStroke();
+        fill("#fffffff");
+    rect(130,200,400,240);
+    pop(); 
+    }
+    
+    function greenflag(){
+        push();
+        noStroke();
+        fill("#00ff00");
+        rect(110,200,160,240);
+        pop();
+    }
+    
+    function redflag(){
+      push();
+      noStroke();
+      fill("#ff0000");
+      rect(380,200,160,240);
+      pop();
+    }
