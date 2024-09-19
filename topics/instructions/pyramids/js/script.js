025f509f348c009e9b2f391f73d64625004ce63e
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-19 13:14:26 -0400

    pyramid

--- topics/instructions/pyramids/js/script.js
+++ topics/instructions/pyramids/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Title of Project
+ * Title of Project 
  * Author Name
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(400,400);
 }
 
 
@@ -20,5 +20,41 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+background("#0000ff");
+  
+  sand();
+  pyramid2();
+pyramid();
 
+}
+
+function sand(){
+    push();
+    noStroke();
+    fill("yellow");
+    ellipse(100,300,300);
+    pop();
+  
+  //second one
+   push();
+    noStroke();
+    fill("yellow");
+    ellipse(300,300,300);
+    pop();
+}
+
+function pyramid(){
+  push();
+  noStroke();
+  fill("#fee12b");
+  triangle(130, 275, 220, 120, 286, 275);
+  pop();
+}
+
+function pyramid2(){
+  push();
+  noStroke();
+  fill("#effd5f");
+  triangle(230,375,320,220,386,375);
+  pop();
 }
