6929c843528bc5a3759c8b1430dd11a720caaedb
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-18 23:00:03 -0400

    started the movement

--- topics/variables/Variable in practice movement/js/script.js
+++ topics/variables/Variable in practice movement/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * Variable in practice: Movement
+ * Emma Beldick
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * experimenting with movement
  */
 
 "use strict";
