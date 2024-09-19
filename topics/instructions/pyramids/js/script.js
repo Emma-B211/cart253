1a9000bef75b668423420deceeb2ce34940a9728
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-19 13:25:23 -0400

    pyramid fixed

--- topics/instructions/pyramids/js/script.js
+++ topics/instructions/pyramids/js/script.js
@@ -1,11 +1,9 @@
 /**
-<<<<<<< HEAD
  * Title of Project 
  * Author Name
-=======
  * Lovely Pyramid
  * Emma Beldick
->>>>>>> a6b623cdc02babcee86f18db32f6fb69a2b3fc09
+
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -17,7 +15,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-    createCanvas(400,400);
+  createCanvas(400, 400);
 }
 
 
@@ -25,38 +23,30 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
-background("#0000ff");
-<<<<<<< HEAD
-  
+  background("#0000ff");
   sand();
   pyramid2();
-pyramid();
+  pyramid();
 
-=======
-
-  sand()
-pyramid()
-pyramid2()
->>>>>>> a6b623cdc02babcee86f18db32f6fb69a2b3fc09
 }
 
-function sand(){
-    push();
-    noStroke();
-    fill("yellow");
-    ellipse(100,300,300);
-    pop();
-  
+function sand() {
+  push();
+  noStroke();
+  fill("yellow");
+  ellipse(100, 300, 300);
+  pop();
+
   //second one
-   push();
-    noStroke();
-    fill("yellow");
-    ellipse(300,300,300);
-    pop();
+  push();
+  noStroke();
+  fill("yellow");
+  ellipse(300, 300, 300);
+  pop();
 }
 
-function pyramid(){
-<<<<<<< HEAD
+function pyramid() {
+
   push();
   noStroke();
   fill("#fee12b");
@@ -64,25 +54,24 @@ function pyramid(){
   pop();
 }
 
-function pyramid2(){
+function pyramid2() {
   push();
   noStroke();
   fill("#effd5f");
-  triangle(230,375,320,220,386,375);
+  triangle(230, 375, 320, 220, 386, 375);
   pop();
 }
-=======
-    push();
+push();
+noStroke();
+fill("#fee12b");
+triangle(175, 120, 108, 230, 236, 225);
+pop();
+
+function pyramid2() {
+  push();
   noStroke();
-  fill("#fee12b");
-  triangle(175,120,108,230,236,225);
+  fill("#ffffc5")
+  triangle(175, 120, 108, 230, 145, 235);
   pop();
 }
-function pyramid2(){
-    push();
-    noStroke();
-    fill("#ffffc5")
-    triangle(175,120,108,230,145,235);
-    pop();
-}
->>>>>>> a6b623cdc02babcee86f18db32f6fb69a2b3fc09
+
