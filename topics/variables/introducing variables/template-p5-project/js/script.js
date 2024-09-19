0c1b4de8c90e1dea279a4b83ec3744ff53853fb5
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-18 20:47:47 -0400

    used variables

--- topics/variables/introducing variables/template-p5-project/js/script.js
+++ topics/variables/introducing variables/template-p5-project/js/script.js
@@ -12,21 +12,21 @@
 */
 function setup() {
 
-createCanvas(640,640);
-
-}
-
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
-function draw() {
-    background(0);
-
-    // draw a circle
-    PushManager();
-    FileList(255,255,0);
-    noStroke();
-    ellipse(320,320,100,100);
-
-}
+    createCanvas(640,640);
+    
+    }
+    
+    
+    /**
+     * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+    */
+    function draw() {
+        background(0);
+    
+        // draw a circle
+        push();
+        fill(255,255,0);
+        noStroke();
+        ellipse(width/2,height/2,mouseX,mouseY);
+    pop();
+    }
