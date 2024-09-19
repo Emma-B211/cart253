f7aeb31b0d6f521a87e7c0db8b8a41c02b56783e
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-19 14:17:29 -0400

    update variable debugging challenge

--- topics/instructions/variables-debugging-challenge/js/script.js
+++ topics/instructions/variables-debugging-challenge/js/script.js
@@ -9,34 +9,45 @@
 
 "use strict";
 
+//let random(-2,2);
+// let createCanvas
+// let background
 // The artist's brush
 // Full of settings we can change over time with random()
 const brush = {
     x: 250,
+    y: 250,
     size: 5,
     fill: {
         r: 127,
         g: 127,
-        b: 127
+        b: 127,
 
-    };
+    }
+}
+/**
+ * Creates the canvas
+ */
+function setup() {
+    createCanvas(500, 500);
 
-    /**
-     * Creates the canvas
-     */
-    function setup() {
-        createCanvas(500, 500);
+}
+
 
 // A one time background fill
-background("#fffffe");
-}
+
+// random not define
+
+
 
 /**
  * Updates and draws the current bit of paint
  */
 function draw() {
-    updateBrush();
+    background("#fffffe");
+
     drawBrush();
+    updateBrush();
 }
 
 function updateBrush() {
@@ -44,9 +55,9 @@ function updateBrush() {
     brush.x += random(-3, 3);
 
     // Update fill randomly
-    brush.fill.red += random(-2, 2);
-    brush.fill.green += random(-2, 2);
-    brush.fill.blue += random(-2, 2);
+    brush.fill.r += random(-2, 2);
+    brush.fill.g += random(-2, 2);
+    brush.fill.b += random(-2, 2);
 
     // Update brush size randomly
     brush.size += random(-1, 1);
@@ -60,6 +71,6 @@ function drawBrush() {
     push();
     noStroke();
     fill(brush.fill.r, brush.fill.g, brush.fill.b);
-    ellipse(brush.x, brush.y, brush.diameter);
+    ellipse(brush.x, brush.y, brush.size);
     pop();
 }
