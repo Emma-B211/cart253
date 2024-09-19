4a26460231c4747fa940e830682f6eaab8dfb5b7
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-19 13:57:01 -0400

    started mr.Furious

--- topics/instructions/mr.furious/js/script.js
+++ topics/instructions/mr.furious/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Title of Project
+ * Mr.Furious
  * Author Name
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
@@ -11,14 +11,36 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
-function setup() {
 
+let mrFurious = {
+    // position and size
+    x: 200,
+    y: 200,
+    size: 100,
+    // colour
+    fill: {
+        r: 255,
+        g: 255,
+        b: 255
+    }
+};
+function setup() {
+    createCanvas(400, 400);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draw (and update) Mr.Furious
 */
 function draw() {
+    background(160, 180, 200);
 
+    // draw mr.furious as a coloured circle
+
+    push();
+    noStroke();
+    fill(mrFurious.r, mrFurious.b, mrFurious.g);
+    ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+    pop();
+   
 }
