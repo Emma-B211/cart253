94bcf976af0634dc5800418b7efa1f85bd7c587f
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-25 20:27:30 -0400

    condition learned

---
+++ condition/template-p5-project/js/libraries/condition.js
@@ -0,0 +1,59 @@
+//if there is freezing rain, then im going to stay at home.
+
+
+if (thereIsFreezingRain) {
+    stayAtHome();
+}
+
+if (condition) {
+    consequence();
+}
+
+if (mouseX > 100) {
+
+}
+
+if (mouseY < height) {
+
+}
+
+if (age === 33) {
+
+}
+
+if (confidence >= 90) {
+
+}
+
+if (sadness <= 10) {
+
+}
+
+if (age !== 12) {
+
+}
+
+//if there is freezing rain, then i'm going to stay at home.
+// other wise go to the cafe
+
+if (thereIsFreezingRain) {
+    stayAtHome();
+}
+else {
+    goToTheCafe();
+}
+
+// if there is freezing rain, then im going to stay at home
+// otherwise, if its 12 degrees or warmer, ill go for a walk.
+// otherwise, i'll go to the cafe.
+
+if (thereISFreezingRain) {
+    stayAtHome();
+}
+else if (temperature >= 12) {
+    goForAWalk();
+}
+else {
+    goToTheCafe();
+
+}
