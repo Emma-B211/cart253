02aad3d5e605fc80e3c37c5bdb8dcba2b818a399
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-25 22:41:19 -0400

    finished death by boredom

--- condition/death-by-boredom/js/script.js
+++ condition/death-by-boredom/js/script.js
@@ -25,7 +25,13 @@ const creature = {
         happy: "#33cc33", // Green
         angry: "#cc3333", // Red
         dead: "#777777" // Grey
-    }
+    },
+    // is the creature alive?
+    alive: true,
+    // how bored is the creature?
+    boredomLevel: 0,
+    // how bored can the creature get before it dies?
+    deathByBoredomThreshold: 500
 };
 
 /**
@@ -45,14 +51,18 @@ function draw() {
     drawCreature();
 }
 
-
-/**
- * Creature is happy if being massaged and otherwise bored
- */
 /**
  * Creature is happy if being massaged and otherwise bored
  */
 function checkInput() {
+    // check if the creature is dead, and if so, don't
+    //check for input (massage)
+    if (!creature.alive) {
+        return;
+    }
+
+
+
     // Calculate the distance between the cursor and the creature
     // and put it into a "distance" variable (using const again since
     // we won't change this again later!)
@@ -73,6 +83,14 @@ function checkInput() {
     else {
         // Otherwise the creature is bored
         creature.fill = creature.fills.bored;
+        //creature gets a lil bored
+        creature.boredomLevel += 1;
+        if (creature.boredomLevel > creature.deathByBoredomThreshold) {
+            // creature dies of boredom
+            creature.alive = false;
+            // creature looks dead
+            creature.fill = creature.fills.dead;
+        }
     }
 }
 
