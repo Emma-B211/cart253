3d2d8ef272519882c27432daf1c3ce12f623e693
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-25 23:51:21 -0400

    started condition challenge

--- condition/template-p5-project/js/script.js
+++ condition/template-p5-project/js/script.js
@@ -1,36 +1,68 @@
+/**
+ * Circle Master
+ * Emma Beldick
+ * 
+ * This will be a program in which the user can move a circle
+ * on the canvas using their own circle to "lead" it around.
+ */
 
+const puck = {
+    x: 350,
+    y: 350,
+    size: 100,
+    fill: "#ff0000"
+};
 
+const user = {
+    x: undefined, // will be mouseX
+    y: undefined, // will be mouseY
+    size: 75,
+    fill: "000000"
+};
 
-// if there is freezing rain, then i'm going to stay at home.
-
-if (thereIsFreezingRain){
-    stayAtHome();
+/**
+ * create a canvas
+ */
+function setup() {
+    createCanvas(400, 400);
 }
 
-if (condition){
-    consequence();
+/**
+ * move the user circle, check for overlap, draw the two circles
+ */
+
+function draw() {
+    background("#aaaaaa");
+
+    // draw the user and pack
+    drawUser();
+    drawPuck();
 }
-
-if (mouseX > 100){
-
-}
-
-if (mouseY < height){
-     
-}
-
-if (age === 33){
-
-}
-
-if (confidence >= 90){
-
-}
-
-if (sadness <= 10 ) {
+/**
+ * sets the user position to the mouse position
+ */
+function moveUser() {
+    user.x = mouseX;
+    user.y = mouseY;
 
 }
-
-if (age !== 12){
-    
+/**
+ * display the user circle
+ */
+function drawUser() {
+    push();
+    noStroke();
+    fill(puck.fill);
+    ellipse(user.x, user.y, user.size);
+    pop();
+}
+/**
+ * draw the puck circle
+ */
+function drawPuck() {
+    push();
+    noStroke();
+    fill(puck.fill);
+    ellipse(puck.x, puck.y, puck.size);
+    pop();
 }
