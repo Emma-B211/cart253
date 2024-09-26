00efeb3d3e38e390f3d394fe4df1fab9123f664c
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-26 14:50:22 -0400

    started art jam

--- condition/art_jam/js/script.js
+++ condition/art_jam/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Excited Dog
+ * Emma Beldick
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(500, 500);
 }
 
 
@@ -20,5 +20,30 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(0, 150, 155);
+    field();
+    field2();
+    field3();
+}
+function field() {
+    push();
+    noStroke()
+    fill(0, 105, 30);
+    ellipse(500, 400, 400);
+    pop();
+}
+function field2() {
+    push();
+    noStroke();
+    fill(0, 125, 50);
+    ellipse(300, 400, 400);
+    pop();
+}
+function field3() {
+    push();
+    noStroke();
+    fill(0, 105, 30);
+    ellipse(100, 400, 400);
+    pop();
 
 }
