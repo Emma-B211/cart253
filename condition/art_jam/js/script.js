e19a8c13a3d6a9b587f266172b1ae14412a9999a
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-09-29 20:25:33 -0400

    working on growing flower

--- condition/art_jam/js/script.js
+++ condition/art_jam/js/script.js
@@ -1,5 +1,5 @@
 /**
- * Excited Dog
+ * Growing Flower
  * Emma Beldick
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
@@ -11,8 +11,15 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
+// const cloud = {
+//     x: 100,
+//     y: 100,
+//     size: 50,
+//     fills: ("#ffffff")
+
+// }
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(600, 600);
 }
 
 
@@ -21,6 +28,9 @@ function setup() {
 */
 function draw() {
     background(0, 150, 155);
+    cloud();
+    clouds2();
+    clouds3();
     field();
     field2();
     field3();
@@ -29,21 +39,81 @@ function field() {
     push();
     noStroke()
     fill(0, 105, 30);
-    ellipse(500, 400, 400);
+    ellipse(500, 500, 400);
     pop();
 }
 function field2() {
     push();
     noStroke();
     fill(0, 125, 50);
-    ellipse(300, 400, 400);
+    ellipse(300, 500, 400);
     pop();
 }
 function field3() {
     push();
     noStroke();
     fill(0, 105, 30);
-    ellipse(100, 400, 400);
+    ellipse(100, 500, 400);
     pop();
 
+}
+
+function cloud() {
+    push();
+    noStroke();
+    fill("#ffffff");
+    ellipse(100, 100, 50);
+    pop();
+
+    push();
+    noStroke();
+    fill("#d3d3d3");
+    ellipse(130, 90, 50);
+    pop();
+
+    push();
+    noStroke();
+    fill("#ffffff");
+    ellipse(150, 100, 50);
+    pop();
+}
+
+function clouds2() {
+    push();
+    noStroke();
+    fill("#ffffff");
+    ellipse(250, 100, 50);
+    pop();
+
+    push();
+    noStroke();
+    fill("#d3d3d3");
+    ellipse(270, 90, 50);
+    pop();
+
+    push()
+    noStroke();
+    fill("#ffffff");
+    ellipse(290, 100, 50);
+    pop()
+}
+
+function clouds3() {
+    push();
+    noStroke();
+    fill("#ffffff");
+    ellipse(380, 100, 50);
+    pop();
+
+    push();
+    noStroke();
+    fill("#d3d3d3");
+    ellipse(400,90,50);
+    pop();
+
+    push();
+    noStroke();
+    fill("#ffffff");
+    ellipse(420,100,50);
+    pop();
 }
