60f8bf8db3b6c69ea27f7c7ce7770d0bac1afa8d
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-10-02 23:05:43 -0400

    added keyboard controls for colour

--- events/keyboard-events/js/script.js
+++ events/keyboard-events/js/script.js
@@ -21,6 +21,11 @@ const ball = {
         white: "#ffffff",
         red: "#ff0000",
         blue: "#0000ff"
+    },
+    // keys to control the colour
+    keys: {
+        redKey: 82, // R
+        blueKey: 66 // B
     }
 }
 
@@ -43,4 +48,25 @@ function draw() {
     fill(ball.fill);
     ellipse(ball.x, ball.y, ball.size);
     pop();
+}
+/**
+ * controls the ball's colour
+ */
+function keyPressed(event) {
+    if (event.keyCode === ball.keys.redKey) {
+        ball.fill = ball.fills.red;
+    }
+    else if (event.keyCode === ball.keys.blueKey) {
+        ball.fill = ball.fills.blue;
+    }
+
+}
+/**
+ * resets the ball's colour
+ */
+function keyReleased(event) {
+    if (event.keyCode === ball.keys.redKey || event.keyCode === ball.keys.blueKey) {
+        ball.fill = ball.fills.white;
+    }
+
 }
