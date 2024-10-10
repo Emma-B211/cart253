37673e158f912d657c589a4240e526d38ca1ada3
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-10-10 13:25:38 -0400

    started making

--- making module/making/js/script.js
+++ making module/making/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Frog
+ * Emma
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -11,8 +11,29 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
-function setup() {
+const frog={
+   //body
+   x: 150,
+   y: 150,
+   size: 90,
+}
 
+const tongue={
+    x:55,
+    y:55,
+    size:40,
+}
+
+const fly = {
+    x: 30,
+    y:40,
+    size:15,
+    speed: +1
+}
+
+
+function setup() {
+createCanvas(400,400);
 }
 
 
@@ -20,5 +41,27 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background("#add8e6");
+   frog();
+    tongue();
+    Fly();
 
+
+}
+
+function Frog(){
+    Push();
+    noStroke();
+    fill("#00ff00");
+    eclipse(frog.x,frog.y,frog.size);
+    pop();
+
+}
+
+function tongue(){
+    push();
+    noStroke();
+    fill("#ff0000");
+    eclipse(tongue.x,tongue.y,tongue.size);
+    pop();
 }
