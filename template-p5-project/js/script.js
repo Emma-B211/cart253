9a4b178d932f18846cb07f4a3d26bf6a8f66a0e7
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-10-31 13:31:51 -0400

    array challenge

--- template-p5-project/js/script.js
+++ template-p5-project/js/script.js
@@ -1,24 +1,88 @@
 /**
- * Title of Project
- * Author Name
- * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Boingo
+ * Pippin Barr
+ *
+ * A ball that bounces around on the canvas
  */
 
-"use strict";
+let ball = undefined; // Will create it with createBall()
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
+ * Create the canvas and the ball
+ */
 function setup() {
-
+  // Create the canvas
+  createCanvas(400, 400);
+  // Create the ball
+  ball = createBall();
 }
 
+/**
+ * Creates a random ball
+ */
+function createBall() {
+  // Create a ball object with appropriate properties
+  const newBall = {
+    // Position and dimensions
+    x: 200,
+    y: 200,
+    size: 20,
+    // Colour
+    fill: "#000000",
+    // Movement
+    velocity: {
+      x: random(-5, 5),
+      y: random(-5, 5)
+    }
+  };
+  return newBall;
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Moves and draws the ball
+ */
 function draw() {
+  background("#87ceeb");
+  
+  moveBall();
+  bounceBall();
+  drawBall();
+}
 
+/**
+ * Moves the ball according to its velocity
+ */
+function moveBall() {
+  ball.x += ball.velocity.x;
+  ball.y += ball.velocity.y;
+}
+
+/**
+ * Bounces the ball off the walls
+ */
+function bounceBall() {
+  // Check if the ball has reached the left or right
+  const bounceX = (ball.x > width || ball.x < 0);
+  // Check if the ball has reached the top or bottom
+  const bounceY = (ball.y > height || ball.y < 0);
+  
+  // Handle bouncing horizontally
+  if (bounceX) {
+    ball.velocity.x *= -1;
+  }
+  // Handle bouncing vertically
+  if (bounceY) {
+    ball.velocity.y *= -1;
+  }
+}
+
+/**
+ * Draw the ball on the canvas
+ */
+function drawBall() {
+  push();
+  noStroke();
+  fill(ball.fill);
+  ellipse(ball.x, ball.y, ball.size);
+  pop();
 }
