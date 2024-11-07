f68a3aad680447ea8ead7949e56516249c73ce89
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-11-07 14:18:24 -0500

    finished loop challenges

--- for loop challenges/js/script.js
+++ for loop challenges/js/script.js
@@ -11,7 +11,7 @@
  * Creates the canvas
  */
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(windowWidth, windowHeight);
 }
 
 /**
@@ -20,26 +20,50 @@ function setup() {
  */
 function draw() {
     background("pink");
+    drawGradient();
+    drawLines();
+    
+    
+
+}
+
+function drawGradient(){
+    //let shade=0;
+    for (let y =0; y<=height;y+=2){
+        let hue=map(y,0,height,0,360)
+        //strokeWeight(0.5);
+        stroke(hue,100,100);
+    line(0,y,width,y);
+    //shade+=1;
+    }
+
+    for(let x =0; x<=width;x++){
+        let hue=map(x,0,width,0,360)
+        //strokeWeight(0.5);
+        stroke(hue,100,100);
+    line(x,0,height,x);
+    //shade+=1;
+    }
+}
+
+function drawLines(){
     let strokeColor=0;
     let strokeStep=1;
     let weight=1;
-    let weightStep=1;
+    let weightStep=0;
     let x=0;
-    letxStep=map(mouseX,0,width,1,true);
+    let xStep= width/11;
 
 while(x<= width){
-   strokeColor(strokeColor);
+   stroke(strokeColor);
     strokeWeight(weight);
     line(x, 0, x, height); 
 
-    x+=xStep;
-    weight+=weightStep;
-    strokeColor+=strokeStep;
+    x += xStep;
+    weight += weightStep;
+    strokeColor += strokeStep;
 
     xStep+=0;
     weightStep +=1;
-};
-    
-    
-
+}
 }
