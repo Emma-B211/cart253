a17d641449c545d241ef7a141f25b724c7726925
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-11-07 13:58:17 -0500

    started challenge

--- for loop challenges/js/script.js
+++ for loop challenges/js/script.js
@@ -20,48 +20,26 @@ function setup() {
  */
 function draw() {
     background("pink");
+    let strokeColor=0;
+    let strokeStep=1;
+    let weight=1;
+    let weightStep=1;
+    let x=0;
+    letxStep=map(mouseX,0,width,1,true);
+
+while(x<= width){
+   strokeColor(strokeColor);
+    strokeWeight(weight);
+    line(x, 0, x, height); 
+
+    x+=xStep;
+    weight+=weightStep;
+    strokeColor+=strokeStep;
+
+    xStep+=0;
+    weightStep +=1;
+};
     
-    stroke(0);
-    strokeWeight(1);
-    line(0, 0, 0, height);
     
-    stroke(25);
-    strokeWeight(2);
-    line(50, 0, 50, height);
-    
-    stroke(50);
-    strokeWeight(3);
-    line(100, 0, 100, height);
-    
-    stroke(75);
-    strokeWeight(4);
-    line(150, 0, 150, height);
-    
-    stroke(100);
-    strokeWeight(5);
-    line(200, 0, 200, height);
-    
-    stroke(125);
-    strokeWeight(6);
-    line(250, 0, 250, height);
-    
-    stroke(150);
-    strokeWeight(7);
-    line(300, 0, 300, height);
-    
-    stroke(175);
-    strokeWeight(8);
-    line(350, 0, 350, height);
-    
-    stroke(200);
-    strokeWeight(9);
-    line(400, 0, 400, height);
-    
-    stroke(225);
-    strokeWeight(10);
-    line(450, 0, 450, height);
-    
-    stroke(250);
-    strokeWeight(11);
-    line(500, 0, 500, height);
+
 }
