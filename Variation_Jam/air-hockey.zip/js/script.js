7f14abeefe8efe88c44b63672bdf502b75adde16
Emma-Beldick <emma.beldick@mail.concordia.ca>
2024-11-14 15:23:49 -0500

    started design of game

--- Variation_Jam/air-hockey.zip/js/script.js
+++ Variation_Jam/air-hockey.zip/js/script.js
@@ -23,5 +23,70 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background("black");
+    backdrop();
+    paddle();
+    paddle1();
+    puck();
+    drawGoal();
+    drawGoal2();
 
+}
+
+function backdrop() {
+    push();
+    noStroke();
+    fill("white");
+    ellipse(400, 400, 250, 250);
+    pop();
+
+    push();
+    noStroke();
+    fill("black");
+    ellipse(400, 400, 200, 200);
+    pop();
+}
+
+
+
+function paddle() {
+    push();
+    noStroke();
+    fill(255, 0, 255);
+    rect(340, 640, 100, 30);
+    rect(300, 650, 180, 40);
+    pop();
+}
+
+function puck() {
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(400, 400, 50, 50);
+    pop();
+}
+
+function paddle1() {
+    push();
+    noStroke();
+    fill(255, 0, 255);
+    rect(340, 140, 100, 50);
+    rect(300, 150, 180, 40);
+    pop();
+}
+
+function drawGoal() {
+    push();
+    noStroke();
+    fill(255, 230, 320);
+    rect(250, 750, 300, 90);
+    pop();
+}
+
+function drawGoal2() {
+    push();
+    noStroke();
+    fill(255, 230, 320);
+    rect(250, 0, 300, 50);
+    pop();
 }
